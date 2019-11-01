73997f304f81bd8e1655735bcf4f2576f8e7d2ed
vincent178 <vh7157@gmail.com>
2019-11-01 17:50:11 +0800

    update application as a http server

--- go/main.go
+++ go/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	log.Fatal(http.ListenAndServe(":80", nil))
 }
