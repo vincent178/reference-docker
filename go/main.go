be32207553c44f4d4a6d95d731d3dbf3156fad2e
vincent178 <vh7157@gmail.com>
2019-11-07 11:45:19 +0800

    add hello world response

--- go/main.go
+++ go/main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello world"))
 	})
 
 	log.Fatal(http.ListenAndServe(":80", nil))
