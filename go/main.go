7014fe95fe929bbe1cdc0a3f4852cb353d7cc97d
vincent178 <vh7157@gmail.com>
2019-11-07 18:11:50 +0800

    update a new version

--- go/main.go
+++ go/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Hello world"))
+		w.Write([]byte("Hello world version"))
 	})
 
 	log.Fatal(http.ListenAndServe(":80", nil))
